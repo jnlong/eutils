ae9dc22215b175f2b3da735faed79f81924aa91a
zhangxianlong <zhangxianlong@baidu.com>
2019-08-06 15:29:17 +0800

    修改兼容性问题

--- src/index.js
+++ src/index.js
@@ -13,9 +13,10 @@ rq.keys().forEach(rqKey => {
     let attr = rqKey.replace('./', '').replace('.js', '');
     if (['tools', 'load', 'type'].indexOf(attr) != -1) {
         tools = rq(rqKey);
-        for (toolsKey in tools) {
-            m[toolsKey] = tools[toolsKey];
-        }
+        console.log(tools)
+        Object.keys(tools).forEach(key => {
+            m[key] = tools[key];
+        })
     } else {
         m[attr] = rq(rqKey);
     }
